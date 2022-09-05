b51c8f86ab02b36b47122adc455abd6e61a178c5
marcosaureliofarias <marcosaureliofarias2023@gmail.com>
2022-09-04 22:27:25 -0300

    casa do codigo

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do codigo e da massa</div>
+      </header>
+    </main>
+  );
+}
